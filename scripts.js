101fd7582ea3fc83c65081dd2297ead822701b01
AatuMett <aatu.mettovaara123@gmail.com>
2023-10-12 12:57:15 +0300

    asdasdasdasd

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 function showAlert() {
     alert('This is a popup alert!');
 }
+
+function showTime() {
+    const timeDisplay = document.getElementById('time-display');
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+    const formattedTime = `${hours}:${minutes}:${seconds}`;
+    timeDisplay.textContent = `Current time is: ${formattedTime}`;
+}
+
+function delayedAlert() {
+    setTimeout(function() {
+        alert('This is a delayed popup alert after 5 seconds.');
+    }, 5000); // 5000 milliseconds = 5 seconds
+}
