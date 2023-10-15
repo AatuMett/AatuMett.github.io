2764082bf16817e517331413d1633b36874e0bb9
AatuMett <aatu.mettovaara123@gmail.com>
2023-10-15 21:57:54 +0300

    lkhogi

--- scripts.js
+++ scripts.js
@@ -15,5 +15,48 @@ function showTime() {
 function delayedAlert() {
     setTimeout(function() {
         alert('This is a delayed popup alert after 5 seconds.');
-    }, 5000); // 5000 milliseconds = 5 seconds
+    }, 5000);
+}
+
+const fruits = ["Apple", "Banana", "Cherry", "Date", "Fig", "Grape", "Kiwi", "Lemon", "Mango", "Orange", "Pineapple", "Strawberry", "Watermelon"];
+
+function getRandomFruit() {
+    const randomIndex = Math.floor(Math.random() * fruits.length);
+    return fruits[randomIndex];
+}
+
+function addRandomFruits() {
+    const fruitContainer = document.getElementById("fruit-container");
+
+    const fruit1 = getRandomFruit();
+    const fruit2 = getRandomFruit();
+
+    fruitContainer.textContent = `Random Fruits: ${fruit1} and ${fruit2}`;
+}
+
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-slide');
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slides[slideIndex].style.display = 'block';
+}
+
+function prevSlide() {
+    showSlide(--slideIndex);
+}
+
+function nextSlide() {
+    showSlide(++slideIndex);
 }
